5e8eeebd405c095f3f260444b839920f8765fb08
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-17 16:08:43 +0530

    [Vikash Prajapati] : added AreaOfTriangle program

---
+++ gcr-codebase/programming-elements/level_2/AreaOfTriangle.java
@@ -0,0 +1,30 @@
+//program to calculate area of triangle
+import java.util.Scanner;
+public class AreaOfTriangle
+{
+    public static void main(String[] args)
+    {
+	//creating variables to store base and height
+	double base, height;
+
+	//Creating Scanner object to take input from user
+	Scanner input=new Scanner(System.in);
+
+	//taking input for base and height
+	System.out.print("Enter base in cm: ");
+	base=input.nextDouble();
+	System.out.print("Enter height in cm: ");
+	height=input.nextDouble();
+	
+	//calculating area of triangle
+	double areaSqCm=0.5*base*height;
+	double areaSqIn=areaSqCm/(2.54*2.54);
+
+	//displaying result
+	System.out.println("The Area of the triangle in sq in is " + areaSqIn +
+                           " and sq cm is " + areaSqCm);
+
+	input.close(); // Closing the Scanner object
+    }
+
+}
