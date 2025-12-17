9f55cc38b180d6c03fb83749aa7b6499130388b9
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-17 12:12:29 +0530

    [Vikash Prajapati] : added HarryAgeCalculator program

---
+++ gcr-codebase/programming-elements/level_1/HarryAgeCalculator.java
@@ -0,0 +1,16 @@
+// Program to calculate Harry's age
+public class HarryAgeCalculator{
+
+    public static void main(String[] args){
+
+        // Declare and initialize variables
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Calculate age
+        int age = currentYear - birthYear;
+
+        // Display the result
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
