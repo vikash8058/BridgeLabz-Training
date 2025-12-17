7d8308b38d48963fee1ca8b6a97c83c6fac97250
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-17 15:49:14 +0530

    [Vikash Prajapati] : added BasicOperation program

---
+++ gcr-codebase/programming-elements/level_2/BasicOperation.java
@@ -0,0 +1,27 @@
+// program to perform basic operation 
+import java.util.Scanner;
+
+public class BasicOperation
+{
+    public static void main(String[] args)
+    {
+	//declare variables
+	double number1, number2;
+
+	Scanner input=new Scanner(System.in);  //Scanner object
+	
+	number1=input.nextDouble();  //taking first number input
+	number2=input.nextDouble();  //taking second number input
+
+	double addition=number1+number2;         //calcualating addition
+	double subtraction=number1-number2;      //calculating subtraction
+	double multiplication=number1*number2;   //calculating multiplication
+	double division=number1/number2;         //calculating division
+	System.out.println("The addition, subtraction, multiplication and division value of 2 numbers " 
+                           + number1 + " and " + number2 + " is " 
+                           + addition + ", " + subtraction + ", " 
+                           + multiplication + ", and " + division); //Display the result
+	
+	input.close();  //closing the Scanner object
+    }
+}
