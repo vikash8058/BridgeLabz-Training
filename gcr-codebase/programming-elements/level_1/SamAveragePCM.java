4a40e7cc79947d07e7629d1ee2d44b451229858c
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-17 12:51:08 +0530

    [Vikash Prajapati] : added SamAveragePCM program

---
+++ gcr-codebase/programming-elements/level_1/SamAveragePCM.java
@@ -0,0 +1,20 @@
+// Program to calculate Sam's average percentage in PCM
+public class SamAveragePCM{
+
+    public static void main(String[] args) {
+
+        // Declare and initialize marks
+        int mathsMarks = 94;
+        int physicsMarks = 95;
+        int chemistryMarks = 96;
+
+        
+        int totalMarks = mathsMarks + physicsMarks + chemistryMarks;      // Calculate total marks
+
+        
+        double averagePercentage = totalMarks / 3.0;     // Calculate average percentage
+
+       
+        System.out.println("Sam’s average mark in PCM is " + averagePercentage);    // Display the result
+    }
+}
