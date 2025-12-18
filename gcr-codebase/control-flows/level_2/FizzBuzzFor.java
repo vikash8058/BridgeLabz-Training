93c87e00b3f9d5996fb6e575e4ec1d1cdaf71f10
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-18 19:20:10 +0530

    [Vikash Prajapati] : added FizzBuzzFor Program

---
+++ gcr-codebase/control-flows/level_2/FizzBuzzFor.java
@@ -0,0 +1,45 @@
+//program to print FizzBuzz using for loop
+import java.util.Scanner;
+
+public class FizzBuzzFor
+{
+    public static void main(String[] args)
+    {
+	//create Scanner object
+	Scanner input=new Scanner(System.in);
+
+	//get input value
+	System.out.print("Enter a number: ");
+	int number=input.nextInt();
+
+	if(number>0)
+	{
+	    //loop and print FizzBuzz
+	    for(int i=1;i<=number;i++)
+	    {
+	        if(i%3==0 && i%5==0)
+	        {
+		    System.out.println("FizzBuzz");
+	        }
+	        else if(i%3==0)
+	        {
+		    System.out.println("Fizz");
+	        }
+	        else if(i%5==0)
+	        {
+		    System.out.println("Buzz");
+	        }
+	        else
+	        {
+		    System.out.println(i);
+	        }
+	    }	
+	}
+	else
+	{
+	    System.out.println(number+" is not positive number");
+	}
+
+	input.close();  //closing input stream
+    }
+}
