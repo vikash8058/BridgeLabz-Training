f28178d3dfa4a624b6099a1ce4635d5aa6916256
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-18 19:23:12 +0530

    [Vikash Prajapati] : added YoungestTallestFriend Program

---
+++ gcr-codebase/control-flows/level_2/YoungestTallestFriend.java
@@ -0,0 +1,57 @@
+//program to find youngest and tallest friend
+import java.util.Scanner;
+
+public class YoungestAndTallestFriend
+{
+    public static void main(String[] args)
+    {
+	//create Scanner object
+	Scanner input=new Scanner(System.in);
+
+	//get age inputs
+	System.out.print("Enter Amar age: ");
+	int amarAge=input.nextInt();
+	System.out.print("Enter Akbar age: ");
+	int akbarAge=input.nextInt();
+	System.out.print("Enter Anthony age: ");
+	int anthonyAge=input.nextInt();
+
+	//get height inputs
+	System.out.print("Enter Amar height: ");
+	int amarHeight=input.nextInt();
+	System.out.print("Enter Akbar height: ");
+	int akbarHeight=input.nextInt();
+	System.out.print("Enter Anthony height: ");
+	int anthonyHeight=input.nextInt();
+
+	//find youngest friend and display the result
+	if(amarAge<akbarAge && amarAge<anthonyAge)
+	{
+	    System.out.println("Youngest Friend: Amar");
+	}
+	else if(akbarAge<amarAge && akbarAge<anthonyAge)
+	{
+	    System.out.println("Youngest Friend: Akbar");
+	}
+	else
+	{
+	    System.out.println("Youngest Friend: Anthony");
+	}
+
+	//find tallest friend and display the result
+	if(amarHeight>akbarHeight && amarHeight>anthonyHeight)
+	{
+	    System.out.println("Tallest Friend: Amar");
+	}
+	else if(akbarHeight>amarHeight && akbarHeight>anthonyHeight)
+	{
+	    System.out.println("Tallest Friend: Akbar");
+	}
+	else
+	{
+	    System.out.println("Tallest Friend: Anthony");
+	}
+
+	input.close();  //closing input stream
+    }
+}
