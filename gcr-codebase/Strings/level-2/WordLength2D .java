6433a6fb9c391399c025ccbb292f4d921dec1a4d
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-22 21:43:27 +0530

    [Vikash Prajapati] : added 2D word length calculation program using strings

---
+++ gcr-codebase/Strings/level-2/WordLength2D .java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class WordLength2D {
+
+    // method to find length without using length()
+    public static int findLength(String text) {
+        int count = 0;
+        while (true) {
+            try {
+                text.charAt(count);
+                count++;
+            } catch (Exception e) {
+                break;
+            }
+        }
+        return count;
+    }
+
+    // method to split text into words without split()
+    public static String[] splitWords(String text) {
+
+        int wordCount = 1;
+        for (int i = 0; i < findLength(text); i++) {
+            if (text.charAt(i) == ' ') {
+                wordCount++;
+            }
+        }
+
+        String[] words = new String[wordCount];
+        int index = 0;
+        String word = "";
+
+        for (int i = 0; i < findLength(text); i++) {
+            if (text.charAt(i) != ' ') {
+                word += text.charAt(i);
+            } else {
+                words[index++] = word;
+                word = "";
+            }
+        }
+        words[index] = word;
+
+        return words;
+    }
+
+    // method to create 2D array of word and length
+    public static String[][] createWordLengthArray(String[] words) {
+
+        String[][] result = new String[words.length][2];
+
+        for (int i = 0; i < words.length; i++) {
+            result[i][0] = words[i];
+            result[i][1] = String.valueOf(findLength(words[i]));
+        }
+
+        return result;
+    }
+
+    // display method
+    public static void display(String[][] data) {
+        System.out.println("\nWord\tLength");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i][0] + "\t" + Integer.parseInt(data[i][1]));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = input.nextLine();
+
+        String[] words = splitWords(text);
+        String[][] result = createWordLengthArray(words);
+
+        display(result);
+
+        input.close();
+    }
+}
