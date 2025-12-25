c9dc74904511730da55036a44f32f1a661a1d9bc
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-25 22:41:02 +0530

    [Vikash Prajapati] : added LeetCode Square Root solution

---
+++ leet-code-codebase/Sqrt(x)_ 69.java
@@ -0,0 +1,54 @@
+/* 69. Sqrt(x)
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+*/
+
+class Solution {
+    public int mySqrt(int x) {
+        
+        //handle edge case
+        if(x == 0 || x == 1) {
+            return x;
+        }
+        
+        int result = 0;
+        
+        //find square root using loop
+        for(int i = 1; i <= x; i++) {
+            
+            if((long)i * i > x) {
+                break;
+            }
+            
+            result = i;
+        }
+        
+        return result;
+    }
+}
