bca7ecef6b7f5b0f9b8d2925f316f1e4744d3b78
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-25 22:40:52 +0530

    [Vikash Prajapati] : added LeetCode Power of Two solution

---
+++ leet-code-codebase/PowerOfTwo_231.java
@@ -0,0 +1,55 @@
+/* 231. Power of Two
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: n = 3
+Output: false
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1
+*/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        //negative numbers and zero cannot be power of two
+        if(n <= 0) {
+            return false;
+        }
+        
+        //keep dividing by 2 until number becomes 1
+        while(n != 1) {
+            
+            //if number is not divisible by 2
+            if(n % 2 != 0) {
+                return false;
+            }
+            
+            n = n / 2;
+        }
+        
+        return true;
+    }
+}
