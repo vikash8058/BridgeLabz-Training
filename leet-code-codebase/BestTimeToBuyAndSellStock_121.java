8fa3a33a24cf6338051651d34fdd9737a40d53d2
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-28 17:27:06 +0530

    [Vikash Prajapati] : added LeetCode Best Time to Buy and Sell Stock solution

---
+++ leet-code-codebase/BestTimeToBuyAndSellStock_121.java
@@ -0,0 +1,45 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int minimumBuyPrice = prices[0];
+        int maximumProfit = 0;
+        
+        // Traverse prices starting from day 1
+        for (int i = 1; i < prices.length; i++) {
+            
+            // Update minimum buying price if a lower price is found
+            if (prices[i] < minimumBuyPrice) {
+                minimumBuyPrice = prices[i];
+            } 
+            // Calculate profit if selling on current day
+            else {
+                int currentProfit = prices[i] - minimumBuyPrice;
+                maximumProfit = Math.max(maximumProfit, currentProfit);
+            }
+        }
+        
+        return maximumProfit;
+    }
+}
