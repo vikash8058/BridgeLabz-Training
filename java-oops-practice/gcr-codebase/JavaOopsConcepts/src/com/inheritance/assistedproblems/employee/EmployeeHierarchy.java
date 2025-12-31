74245d6fb71b26bd6bedfda1b607d1895417c67e
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2025-12-31 15:32:26 +0530

    [Vikash Prajapati] : modified employee hierarchy program

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/EmployeeHierarchy.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/EmployeeHierarchy.java
@@ -2,16 +2,15 @@ package com.inheritance.assistedproblems.employee;
 
 public class EmployeeHierarchy {
 	public static void main(String[] args) {
-		
-		Employees manager=new Managers("Manager", 101, 75000.50);  //creating object of manager class
+
+		Employees manager = new Managers("Manager", 101, 75000.50); // creating object of manager class
 		manager.displayDetails();
-		
-		Employees developer=new Developers("Developer", 102, 70000.); //creating object of developer class
+
+		Employees developer = new Developers("Developer", 102, 70000.); // creating object of developer class
 		developer.displayDetails();
-		
-		Employees intern=new Interns("Intern", 103, 5000);  //creating object of intern class
+
+		Employees intern = new Interns("Intern", 103, 5000); // creating object of intern class
 		intern.displayDetails();
-		
-		
+
 	}
 }
