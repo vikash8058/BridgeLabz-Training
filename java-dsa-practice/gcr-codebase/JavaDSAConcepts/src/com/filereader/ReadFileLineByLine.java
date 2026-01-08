b7a4cd9e51bc4ce8d99f513fcfd0f3c30061a401
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-01-08 11:52:53 +0530

    [Vikash Prajapati] : added read dile line by line using filereader program

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/filereader/ReadFileLineByLine.java
@@ -0,0 +1,22 @@
+package com.filereader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileLineByLine {
+	public static void main(String[] args) throws IOException {
+
+		// defining the path of file which i want to read
+		String filePath = "D:/HCL/javaquestionset2.txt";
+
+		// buffered reader and file reader for handling a file
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+
+		String line;
+
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println(line);
+		}
+	}
+}
