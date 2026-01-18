9e39515c9937eea4d2c206b820a47b4cda71ddb2
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-01-18 18:16:22 +0530

    [Vikash Prajapati] : added invert map program using map interface

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/mapinterface/InvertMap.java
@@ -0,0 +1,24 @@
+package com.mapinterface;
+
+import java.util.*;
+
+public class InvertMap {
+
+	public static void main(String[] args) {
+
+		Map<String, Integer> map = new HashMap<>();
+		map.put("A", 1);
+		map.put("B", 2);
+		map.put("C", 1);
+
+		Map<Integer, List<String>> inverted = new HashMap<>();
+
+		for (String key : map.keySet()) {
+			int value = map.get(key);
+
+			inverted.computeIfAbsent(value, k -> new ArrayList<>()).add(key);
+		}
+
+		System.out.println(inverted);
+	}
+}
