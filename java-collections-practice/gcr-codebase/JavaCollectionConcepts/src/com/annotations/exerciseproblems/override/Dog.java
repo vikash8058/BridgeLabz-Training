a78a9ad30472e0aa77dc3f5b59cfa9cd0534684b
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-01-25 11:06:51 +0530

    [Vikash Prajapati] : added override annotation exercise problems

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/annotations/exerciseproblems/override/Dog.java
@@ -0,0 +1,9 @@
+package com.annotations.exerciseproblems.override;
+
+public class Dog extends Animal {
+
+	@Override
+	public void makeSound() {
+		System.out.println("Dog barks");
+	}
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/annotations/exerciseproblems/override/MainApp.java
@@ -0,0 +1,10 @@
+package com.annotations.exerciseproblems.override;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		Animal animal = new Dog(); // Polymorphism
+		animal.makeSound();
+	}
+}
