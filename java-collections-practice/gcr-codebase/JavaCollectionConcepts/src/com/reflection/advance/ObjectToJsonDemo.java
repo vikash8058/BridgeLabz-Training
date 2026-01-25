6bca82d7e41da68db1d23f5f3c9b29ee2391a389
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-01-25 11:02:17 +0530

    [Vikash Prajapati] : added advanced level reflection examples

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/reflection/advance/ObjectToJsonDemo.java
@@ -0,0 +1,66 @@
+package com.reflection.advance;
+
+import java.lang.reflect.Field;
+
+public class ObjectToJsonDemo {
+
+	// 🔹 Convert Object to JSON-like String
+	public static String toJson(Object obj) {
+
+		if (obj == null)
+			return "null";
+
+		Class<?> cls = obj.getClass();
+		Field[] fields = cls.getDeclaredFields();
+
+		StringBuilder json = new StringBuilder();
+		json.append("{");
+
+		try {
+			for (int i = 0; i < fields.length; i++) {
+				Field field = fields[i];
+				field.setAccessible(true);
+
+				json.append("\"").append(field.getName()).append("\":");
+
+				Object value = field.get(obj);
+
+				if (value instanceof String) {
+					json.append("\"").append(value).append("\"");
+				} else {
+					json.append(value);
+				}
+
+				if (i < fields.length - 1) {
+					json.append(", ");
+				}
+			}
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		json.append("}");
+		return json.toString();
+	}
+
+	// 🔹 Sample class
+	static class Student {
+		private int id;
+		private String name;
+		private boolean active;
+
+		Student(int id, String name, boolean active) {
+			this.id = id;
+			this.name = name;
+			this.active = active;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Student student = new Student(101, "Anuj", true);
+
+		String json = toJson(student);
+		System.out.println(json);
+	}
+}
