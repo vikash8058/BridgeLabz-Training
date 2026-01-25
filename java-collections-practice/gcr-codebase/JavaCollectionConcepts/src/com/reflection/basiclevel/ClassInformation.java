2e00dec036bf0e0372d2591a8bdfb653d89402d3
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-01-25 10:56:36 +0530

    [Vikash Prajapati] : added classInformation problem of reflection

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/reflection/basiclevel/ClassInformation.java
@@ -0,0 +1,46 @@
+package com.reflection.basiclevel;
+
+import java.lang.reflect.*;
+import java.util.Scanner;
+
+public class ClassInformation {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter fully qualified class name: ");
+		String className = sc.nextLine();
+
+		try {
+			Class<?> cls = Class.forName(className);
+
+			System.out.println("\nClass Name: " + cls.getName());
+
+			// 🔹 Constructors
+			System.out.println("\nConstructors:");
+			Constructor<?>[] constructors = cls.getDeclaredConstructors();
+			for (Constructor<?> c : constructors) {
+				System.out.println(c);
+			}
+
+			//  Fields
+			System.out.println("\nFields:");
+			Field[] fields = cls.getDeclaredFields();
+			for (Field f : fields) {
+				System.out.println(f);
+			}
+
+			//  Methods
+			System.out.println("\nMethods:");
+			Method[] methods = cls.getDeclaredMethods();
+			for (Method m : methods) {
+				System.out.println(m);
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Class not found! Please enter a valid class name.");
+		}
+
+		sc.close();
+	}
+}
