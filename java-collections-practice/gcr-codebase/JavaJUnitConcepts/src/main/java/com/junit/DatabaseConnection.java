d7ddb3a33a1fc9531427b55f908b088a26c334fe
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-01-27 14:20:32 +0530

    [Vikash Prajapati] : added Database connection program using junit

---
+++ java-collections-practice/gcr-codebase/JavaJUnitConcepts/src/main/java/com/junit/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package com.junit;
+
+public class DatabaseConnection {
+
+	private boolean connected;
+
+	public void connect() {
+		connected = true;
+	}
+
+	public void disconnect() {
+		connected = false;
+	}
+
+	public boolean isConnected() {
+		return connected;
+	}
+}
