1554460795cda58f7c5690ad4feb543d64585ac2
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-01-27 21:00:03 +0530

    [Vikash Prajapati] : updated smart home lighting automation using lambda expressions

--- java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/lambda/smarthomelight/SmartHomeLighting.java
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/lambda/smarthomelight/SmartHomeLighting.java
@@ -1,18 +1,23 @@
 package com.lambda.smarthomelight;
-
 public class SmartHomeLighting {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SmartLightController controller = new SmartLightController();
+        SmartLightController controller = new SmartLightController();
 
-		// Motion detected
-		controller.activateLight(() -> System.out.println("Motion detected: Turning ON white lights"));
+        //  Motion detected
+        controller.activateLight(() -> 
+            System.out.println("Motion detected: Turning ON white lights")
+        );
 
-		// Night time
-		controller.activateLight(() -> System.out.println("Night time: Dimming lights to 30%"));
+        //  Night time
+        controller.activateLight(() -> 
+            System.out.println("Night time: Dimming lights to 30%")
+        );
 
-		// Voice command
-		controller.activateLight(() -> System.out.println("Voice command received: Changing lights to BLUE"));
-	}
+        //  Voice command
+        controller.activateLight(() -> 
+            System.out.println("Voice command received: Changing lights to BLUE")
+        );
+    }
 }
