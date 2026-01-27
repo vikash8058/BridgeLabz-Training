c3230a1c3213493a162b39cb57ed58cc412b47b0
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-01-27 14:18:56 +0530

    [Vikash Prajapati] : added Calculator program using junit

---
+++ java-collections-practice/gcr-codebase/JavaJUnitConcepts/src/main/java/com/junit/Calculator.java
@@ -0,0 +1,23 @@
+package com.junit;
+public class Calculator {
+
+    // add two numbers
+    public int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    // subtract two numbers
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    // multiply two numbers
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    // divide two numbers
+    public int divide(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+}
